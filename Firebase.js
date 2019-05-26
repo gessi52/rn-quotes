d5f708a40a3a955224b6c672d4477d0b2db8640c
gessi52 <51041020+gessi52@users.noreply.github.com>
2019-05-26 07:35:47 +0200

    Update Firebase.js

--- Firebase.js
+++ Firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDOVcKCKkDaXoQI7KDPFEfswfolcDrIkFY",
-    authDomain: "quotes-4ea75.firebaseapp.com",
-    databaseURL: "https://quotes-4ea75.firebaseio.com",
-    projectId: "quotes-4ea75",
-    storageBucket: "quotes-4ea75.appspot.com",
-    messagingSenderId: "234480386439",
-    appId: "1:234480386439:web:f5a0a59542e28e85"};
+    apiKey: "xxx",
+    authDomain: "xxx",
+    databaseURL: "xxx",
+    projectId: "xxx",
+    storageBucket: "xxx",
+    messagingSenderId: "xxx",
+    appId: "xxx"};
 
     export default class Firebase {
         static db;
